5c5a76ccfa2cb255959cd2a2c5b1822db830aa92
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-07-22 01:11:42 +0900

    캐러셀 작성

--- client/app/components/Carousel.tsx
+++ client/app/components/Carousel.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import React, { MutableRefObject, RefObject, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import SvgIcon from "@mui/material/SvgIcon";
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
@@ -15,44 +15,46 @@ type Props = {
 } 
 
 const Carousel: React.FC<Props> = ({images}) => {
+  const [currIndex, setCurrIndex] = useState(0);
+  const carouselRef: any = useRef<HTMLDivElement>();
+  const [currList, setCurrList] = useState<CarouselItem[]>([]);
 
-  const [current, setCurrent] = useState(0);
-
-  const moveStyle: { [key: number]: string } = {
-    0: 'translate-x-0',
-    1: 'translate-x-[-55vw]',
-    2: 'translate-x-[-110vw]',
-    3: 'translate-x-[-3000vw]',
-    4: 'translate-x-[-4000vw]',
-    5: 'translate-x-[-5000vw]',
-    6: 'translate-x-[-600vw]',
-    7: 'translate-x-[-700vw]',
-    8: 'translate-x-[-800vw]',
-    9: 'translate-x-[-900vw]',
-    10: 'translate-x-[-1000vw]',
-  };
-
-//다음 버튼을 누르면 current를 1 증가시킴, 만약 현재 마지막 사진이면 첫번째 사진으로 가도록 0 return
-  const nextHandler = () => {
-    setCurrent(() => {
-      if (current === images.length - 1) {
-        return 0;
-      } else {
-        return current + 1;
+  useEffect(() => {
+    if (images.length !== 0) {
+      const startData = [images[0], images[1]];
+      const endData = [images[images.length - 1], images[images.length - 2]];
+      const newList = [...endData, ...images, ...startData];
+      setCurrList(newList)
+    }
+  }, [images]);
+  
+  const moveToNthSlide = (index: number) => {
+    setTimeout(() => {
+      setCurrIndex(index)
+      if (carouselRef.current !== null) {
+        carouselRef.current.style.transition = ''
+        console.log( carouselRef.current.style.transition, '초기화');
       }
-    });
-  };
+    }, 200)
+  }
 
-  //이전 버튼을 누르면 current를 1 감소시킴, 만약 현재 첫번째 사진이면 마지막 사진으로 가도록 마지막사진 인덱스 return
-  const prevHandler = () => {
-    setCurrent(() => {
-      if (current === 0) {
-        return images.length - 1;
-      } else {
-        return current - 1;
-      }
-    });
-  };
+  const handleSwipe = (direction: number) => {
+    const newIndex = currIndex + direction
+    console.log(newIndex,'확인');
+    if (newIndex === currList.length - 3) {
+      moveToNthSlide(0);
+    }  
+    else if (newIndex === -3) {
+      console.log(newIndex,'0으로들어옴');
+      moveToNthSlide(0);
+    }
+
+    setCurrIndex((prev) => prev + direction);
+    
+    if (carouselRef.current !== null) {
+      carouselRef.current.style.transition = 'all 0.5s ease-in-out';
+    }
+  }
 
   //자동으로 캐러셀 움직이게 2초마다 nextHandler를 호출
   // useEffect(() => {
@@ -63,17 +65,25 @@ const Carousel: React.FC<Props> = ({images}) => {
   //   }
   // }, [current])
 
+  useEffect(() => {
+    if (carouselRef.current !== null) {
+      carouselRef.current.style.transform = `translateX(${currIndex * -1035}px)`;
+      console.log(carouselRef.current.style.transform , '현재');
+    }
+  }, [currIndex])
+
     return (
-      <div className="relative h-5/6">
+      <div className="relative h-[400px] m-auto">
+        <SvgIcon  onClick={() => handleSwipe(-1)} component={ArrowLeftIcon} inheritViewBox  style={{fontSize:'55px'}} className='absolute z-10 transform -translate-y-1/2 left-[5vw] cursor-pointer top-1/2'/>
+        <SvgIcon onClick={() => handleSwipe(1)}  component={ArrowRightIcon} inheritViewBox  style={{fontSize:'55px'}}  className='absolute right-[0vw] z-10 transform -translate-y-1/2 cursor-pointer top-1/2'/>
         {/*사진 넘기는 버튼*/}
-        <SvgIcon onClick={prevHandler} component={ArrowLeftIcon} inheritViewBox  style={{fontSize:'55px'}} className='absolute transform -translate-y-1/2 cursor-pointer top-1/2 '/>
-        <SvgIcon onClick={nextHandler} component={ArrowRightIcon} inheritViewBox  style={{fontSize:'55px'}}  className='absolute right-0 transform -translate-y-1/2 cursor-pointer top-1/2'/>
-        <div className='absolute transform -translate-x-1/2 left-1/2'>
+        <div className='absolute -translate-x-1/2 left-1/2'>
           <div
-            className={`flex ${moveStyle[current]} transition duration-300 ease-in-out `}>
-            {images.map((image , id) => (
-              <div key={id} className="flex justify-center m-auto w-[1000px] mx-4 border-2 border-black h-96  rounded">
-                <img src={image.img} className="border-2 object-conain"/>
+            className={`relative flex  m-auto transition duration-300 ease-in-out `} ref={carouselRef}>
+            {currList?.map((image , id) => (
+              <div key={id} className="flex w-[1024px] mx-4 border-2 h-96 ">
+                
+                <img src={image.img} className="rounded object-conain"/>
                 {/* <div className=''> 
                   <h1>{image.title}</h1>
                   <span>{image.text}</span>
@@ -82,18 +92,15 @@ const Carousel: React.FC<Props> = ({images}) => {
             ))}
           </div>
         </div>
-       
-       
-
         {/* 하단 점, 점과 current가 같다면 투명도를 변경시켜 표시함. 클릭하면 current를 클릭한 index로 변경시킴 */}
         <ul className="absolute flex justify-center w-full gap-4 bottom-5">
           {images.map((_, idx) => (
             <li
               key={idx}
               className={`h-[1.2rem] w-[1.2rem] rounded-full bg-white ${
-                idx === current ? 'opacity-100' : 'opacity-50'
+                idx === currIndex ? 'opacity-100' : 'opacity-50'
               }`}
-              onClick={() => setCurrent(idx)}
+              onClick={() => setCurrIndex(idx)}
             />
           ))}
         </ul>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,9 @@
 import Carousel from "./components/Carousel";
 
 const CaroulselIMG = [
-  {img :'/images/wanted4.webp',title : '이번주 신규 포지션', text : '눈여겨볼 이번 주 채용공고를 소개합니다.'},
   {img :'/images/wanted.webp',title : '라이프스타일 커머스의 새로운 기준', text : '뷰티셀렉션에서 전 직군 채용 중!'},
   {img :'/images/wanted2.webp',title : '요즘 뜨는 로봇 산업 추천 공고', text : '로봇과 함께 하는 조화로운 미래 세상'},
   {img :'/images/wanted4.webp',title : '이번주 신규 포지션', text : '눈여겨볼 이번 주 채용공고를 소개합니다.'},
-  {img :'/images/wanted.webp',title : '라이프스타일 커머스의 새로운 기준', text : '뷰티셀렉션에서 전 직군 채용 중!'},
 ]
 
 const Home = () =>  {
