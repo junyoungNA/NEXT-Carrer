e9292c0efd4a668196b377c3451f2952b5ddcc59
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-16 03:38:21 +0900

    nav 헤더 스타일링 수정 grid 적용 및 navItemp 컴포넌트 적용

--- client/app/components/Navbar/NavItem.tsx
+++ client/app/components/Navbar/NavItem.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavItem:React.FC<{}> = () => {
     return (
-        <div className='font-roboto'>
-            폰트 테스트하기
+        <div className='grid grid-cols-[350px_1fr] gap-28 mx-auto place-items-end'>
+            <ul className='flex'>
+                <li> 
+                    <Link 
+                        href={'/companyList'} 
+                        className="w-20 px-2 pt-1 mr-2 text-lg text-center h-7">
+                            기업
+                    </Link>
+                    <Link 
+                        href={'/companyList'} 
+                        className="w-20 px-2 pt-1 mr-2 text-lg text-center h-7">
+                            커뮤니티
+                    </Link>
+                    <Link 
+                        href={'/companyList'} 
+                        className="w-20 px-2 pt-1 mr-2 text-lg text-center h-7">
+                            이벤트
+                    </Link>
+                </li>
+            </ul>
+            <ul className='flex'>
+                <li>
+                    <button 
+                        className="w-20 px-2 mr-2 text-sm text-center text-white bg-gray-400 rounded h-7"
+                    >
+                        로그아웃
+                    </button>
+                </li>
+                <li> 
+                    <Link
+                        href="/login"
+                        className="w-20 px-2 pt-0.5 mr-2 text-center text-blue-500 border border-blue-500 ronded h-7"
+                    >
+                        로그인
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                    href="/register"
+                    className="w-20 px-2 pt-0.5 text-center text-white bg-gray-400 rounded h-7"
+                    >
+                        회원가입
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }
--- client/app/components/Navbar/Navbar.tsx
+++ client/app/components/Navbar/Navbar.tsx
@@ -5,30 +5,11 @@ import NavItem from "./NavItem";
 const Navbar: React.FC<{}> = () => {
   return (
     <header className= 'relative w-full bg-white border-b-2 font-roboto'>
-      <nav className="z-10 flex items-center justify-between m-auto w-[1024px] h-14">
+      <nav className="z-10 flex items-center mx-auto w-[1024px] h-14">
       <span className="text-2xl font-semibold text-gray-400">
         <Link href="/">NEXT-Carrer</Link>
       </span>
-      <div className="flex ">
-        <Link href={'/companyList'} className="w-20 px-2 pt-1 mr-2 text-lg text-center h-7">기업</Link>
-      </div>
-        <aside className="flex pt-1">
-          <button className="w-20 px-2 mr-2 text-sm text-center text-white bg-gray-400 rounded h-7">
-            로그아웃
-          </button>
-          <Link
-            href="/login"
-            className="w-20 px-2 pt-0.5 mr-2 text-center text-blue-500 border border-blue-500 ronded h-7"
-          >
-            로그인
-          </Link>
-          <Link
-            href="/register"
-            className="w-20 px-2 pt-0.5 text-center text-white bg-gray-400 rounded h-7"
-          >
-            회원가입
-          </Link>
-        </aside>
+        <NavItem/>
       </nav>
     </header>
   );
