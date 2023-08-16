615198708b3fa8a2fa25af5455da6442725be7fe
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-17 02:28:10 +0900

    prisma 설치 및 스키마 설정 prisma에 대해 navbar 스타일 수정

--- client/app/components/Navbar/NavItem.tsx
+++ client/app/components/Navbar/NavItem.tsx
@@ -3,46 +3,43 @@ import Link from 'next/link'
 
 const NavItem:React.FC<{}> = () => {
     return (
-        <div className='grid grid-cols-[350px_1fr] gap-28 mx-auto place-items-end'>
+        <div className='grid grid-cols-[350px_1fr] gap-28 mx-auto place-items-end text-sm'>
             <ul className='flex'>
                 <li> 
                     <Link 
                         href={'/companyList'} 
-                        className="w-20 px-2 pt-1 mr-2 text-lg text-center h-7">
+                        className="w-20 px-2 mr-2 text-center h-7">
                             기업
                     </Link>
                     <Link 
-                        href={'/companyList'} 
-                        className="w-20 px-2 pt-1 mr-2 text-lg text-center h-7">
-                            커뮤니티
+                        href={'/user'} 
+                        className="w-20 px-2 mr-2 text-center h-7">
+                            유저
                     </Link>
                     <Link 
-                        href={'/companyList'} 
-                        className="w-20 px-2 pt-1 mr-2 text-lg text-center h-7">
-                            이벤트
+                        href={'/admin'} 
+                        className="w-20 px-2 mr-2 text-center h-7">
+                            어드민
                     </Link>
                 </li>
             </ul>
-            <ul className='flex'>
-                <li>
-                    <button 
-                        className="w-20 px-2 mr-2 text-sm text-center text-white bg-gray-400 rounded h-7"
-                    >
+            <ul className='flex items-center gap-3'>
+                <li className='w-20 px-2 pt-1 text-center text-blue-500 border border-blue-500 ronded h-7'>
+                    <button>
                         로그아웃
                     </button>
                 </li>
-                <li> 
+                <li className="w-20 px-2 pt-1 text-center text-blue-500 border border-blue-500 ronded h-7"> 
                     <Link
                         href="/login"
-                        className="w-20 px-2 pt-0.5 mr-2 text-center text-blue-500 border border-blue-500 ronded h-7"
+                        
                     >
                         로그인
                     </Link>
                 </li>
-                <li>
+                <li className="w-20 px-2 pt-1 text-center text-white bg-gray-400 rounded h-7">
                     <Link
                     href="/register"
-                    className="w-20 px-2 pt-0.5 text-center text-white bg-gray-400 rounded h-7"
                     >
                         회원가입
                     </Link>
