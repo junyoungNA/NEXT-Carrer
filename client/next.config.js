ab44201c97993b51180dcc55aa359a0babcbfe61
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-22 00:07:06 +0900

    next.config.js cloudinary 경로 설정

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     //이미지 src경로 제한 설정 풀기
     reactStrictMode: true,
     images: {
-        domains: ['localhost', 'www.gravatar.com'], // 이미지를 호스팅하는 도메인을 여기에 추가
+        domains: ['localhost', 'www.gravatar.com', 'res.cloudinary.com'], // 이미지를 호스팅하는 도메인을 여기에 추가
     },
 }
 
