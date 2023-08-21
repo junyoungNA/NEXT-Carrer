5817d1ded19a956ba2acd1dd86ba169c75b76d7a
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-08-21 18:54:12 +0900

    communty upload 페이지 스타일링 ,container , heading 컴포넌트 작성 및 스타일링

--- client/app/community/upload/page.tsx
+++ client/app/community/upload/page.tsx
@@ -1,10 +1,80 @@
-import React from 'react'
+'use client';
+import Input from '@/app/components/Input';
+import Button from '@/app/components/Button';
+import Container from '@/app/components/Container';
+import React, { useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Heading from '@/app/components/Heading';
 
 const CommunityUploadpage = () => {
-    return (
-        <div>
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {register , handleSubmit, setValue, watch, formState : {
+        errors,
+    },
+        reset
+    } = 
+        useForm<FieldValues>({
+        defaultValues: {
+            title:'',
+            description: '',
+            category : '',
+            latitude: 33.5563,
+            longitude:126.79581,
+            imageSrc:'',
+            price:1
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
         
-        </div>
+    }
+    return (
+        <Container>
+            <div
+                className='max-w-screen-lg mx-auto'
+            >
+                <form   
+                    className='flex flex-col gap-8'
+                    onSubmit={handleSubmit(onSubmit)}>
+                    <Heading
+                        title ='Community Upload'
+                        subtitle = 'Please upload your thoughts'
+                    />
+                    <Input
+                        id="title"
+                        label="제목"
+                        disabled= {isLoading}
+                        register = {register}
+                        errors={errors}
+                        required
+                    />
+                    <hr />
+                    <Input
+                        id="description"
+                        label="Description"
+                        disabled= {isLoading}
+                        register = {register}
+                        errors={errors}
+                        required
+                    />
+                    <hr />
+                    <div 
+                        className='
+                        grid
+                        grid-cols-1
+                        md:grid-cols-2
+                        gap-3
+                        max-h-[50vh]
+                        overflow-y-auto
+                        '
+                    >
+                        {/* Category  */}
+                    </div>
+                    <Button label='커뮤니티 생성하기'></Button>
+                </form>
+            </div>
+        </Container>
     )
 }
 
